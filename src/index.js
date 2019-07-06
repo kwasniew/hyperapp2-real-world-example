6ed9b63179e68485a27579ea0ae07ecd4196ca00
kwasniew <kwasniewski.mateusz@gmail.com>
2019-07-06 18:19:07 +0200

    got page.js routing working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import { app } from "./web_modules/hyperapp.js";
 import { html } from "./html.js";
+import {RoutePages} from "./routerSubscription.js";
+import {mapValues} from "./object.js";
 
 const initialState = {
-    page: 'home'
+  page: "home"
 };
+
+const pages = {
+  "/": "home",
+  "/login": "login",
+  "/register": "register",
+  "/settings": "settings",
+  "/editor/:slug": "editor",
+  "/article/:slug": "article",
+  "/profile/:username": "userProfile",
+  "/profile/:username/favorited": "favoriteProfile",
+  "*": "home"
+};
+
+const SetPage = page => (state, params) => ({ ...state, page });
+const routes = mapValues(SetPage)(pages);
+
 
 app({
   init: () => [initialState],
-  view: state => html`<div>${state.page}</div>`,
+  view: state =>
+    html`
+      <div>${state.page} <a href="/register">register</a></div>
+    `,
+  subscriptions: state => [RoutePages({ routes })],
   node: document.getElementById("app")
 });
---
+++ src/routerSubscription.js
@@ -0,0 +1,19 @@
+import page from "./web_modules/page.js";
+
+const router = (dispatch, {routes}) => {
+    const paths = Object.keys(routes);
+    paths.forEach(path => {
+        const route = routes[path];
+        page(path, context => {
+            dispatch(route, context.params);
+        });
+    });
+
+    setTimeout(() => {page.start()}, 0);
+
+    return () => {
+        page.stop();
+    };
+};
+
+export const RoutePages = ({routes}) => [router, {routes}];
