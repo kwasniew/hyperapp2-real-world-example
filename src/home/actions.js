23f5e493c9f9ef4946e09b370eb35bccb332848e
kwasniew <kwasniewski.mateusz@gmail.com>
2019-07-07 16:18:44 +0200

    simplified logic

--- src/home/actions.js
+++ src/home/actions.js
@@ -5,12 +5,17 @@ import { mapValues } from "../shared/object.js";
 
 const API_ROOT = "https://conduit.productionready.io/api";
 
-const SetArticles = (state, { articles }) => ({ ...state, isLoading: false, articles });
+const SetArticles = (state, { articles }) => ({
+  ...state,
+  isLoading: false,
+  articles
+});
 
-export const FetchFeed = path => Http({
+export const FetchFeed = path =>
+  Http({
     url: API_ROOT + path,
     action: SetArticles
-});
+  });
 
 export const FetchGlobalFeed = FetchFeed("/articles");
 export const FetchTagFeed = tag => FetchFeed(`/articles?tag=${tag}`);
@@ -23,32 +28,36 @@ export const FetchTags = Http({
 });
 
 const FetchArticles = state => {
-    const activeFeed = state.feeds.find(feed => feed.type === state.active);
-    if(activeFeed.type === GLOBAL_FEED) {
-        return FetchGlobalFeed;
-    } else if(activeFeed.type === TAG_FEED) {
-        return FetchTagFeed(activeFeed.name);
-    } else {
-        return null;
-    }
+  const activeFeed = state.feeds.find(feed => feed.type === state.active);
+  if (activeFeed.type === GLOBAL_FEED) {
+    return FetchGlobalFeed;
+  } else if (activeFeed.type === TAG_FEED) {
+    return FetchTagFeed(activeFeed.name);
+  } else {
+    return null;
+  }
 };
 
-export const ChangeTab = (state, {name, type}) => {
+export const ChangeTab = (state, { name, type }) => {
   const updateFeed = feed => {
     if (feed.type === type) {
       feed.visible = true;
-    } else {
-      if(feed.type === TAG_FEED) {
-        feed.visible = false;
-      }
+    } else if (feed.type === TAG_FEED) {
+      feed.visible = false;
     }
-    if(name) {
+    if (name) {
       feed.name = name;
     }
     return feed;
   };
   const feeds = state.feeds.map(updateFeed);
-  const newState = { ...state, active: type, feeds, articles: [], isLoading: true };
+  const newState = {
+    ...state,
+    active: type,
+    feeds,
+    articles: [],
+    isLoading: true
+  };
   return [newState, [preventDefault, FetchArticles(newState)]];
 };
 
