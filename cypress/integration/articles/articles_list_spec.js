b3b5375e3d51ea3bed55f385373e16186d7b656f
kwasniew <kwasniewski.mateusz@gmail.com>
2019-07-12 14:55:51 +0200

    experiments with tests cnt'd

--- cypress/integration/articles/articles_list_spec.js
+++ cypress/integration/articles/articles_list_spec.js
@@ -29,25 +29,125 @@ const setupGlobalFeed = () => {
     response: "fixture:articles/global.json"
   });
 };
-const article = index => cy.get(".article-preview").eq(index);
+// const article = index => cy.get(".article-preview").eq(index);
 
-// const favoriteButton = {
-//   isFavorited() {},
-//   getCount() {}
+// const favoriteButton = parent => {
+//   const object = parent.find("[data-test='favorite-count']");
+//   const selectors = {
+//     isUnfavorited() {
+//       return object.should("have.class", "unfavorited");
+//     },
+//     isFavorited() {
+//       return object.should("have.class", "favorited");
+//     }
+//   };
+//   return Object.assign(object, selectors);
 // };
 //
-// const meta = () => ({
-//   avatar() {},
-//   author() {},
-//   date() {}
-// });
+// const meta = parent => {
+//   const object = parent.find(".article-meta");
+//   const selectors = {
+//     avatar() {
+//       return object.find("img");
+//     },
+//     author() {
+//       return object.find(".author");
+//     },
+//     date() {
+//       return object.find(".date");
+//     }
+//   };
+//   return Object.assign(object, selectors);
+// };
 //
-// const article = index => ({
-//   meta() {},
-//   title() {},
-//   description() {},
-//   favoriteButton() {}
-// });
+// const article = index => {
+//   const object = cy.get(".article-preview").eq(index);
+//   const selectors = {
+//     meta() {
+//       return meta(object);
+//     },
+//     title() {},
+//     description() {},
+//     favoriteButton() {
+//       return favoriteButton(object);
+//     }
+//   };
+//
+//   return Object.assign(object, selectors);
+// };
+//
+// const articleList = index => {
+//   return {
+//     selector: `.article-preview:eq(${index})`,
+//     meta: {
+//       selector: ".article-meta",
+//       avatar: { selector: "img" },
+//       author: { selector: ".author" },
+//       date: { selector: ".date" }
+//     },
+//     favoriteButton: {
+//       selector: "[data-test='favorite-count']"
+//     }
+//   };
+// };
+
+const articleFromList = index => {
+  const indexed = name => `${name}-${index}`;
+  const indexedArticle = indexed("article");
+  const article = cy.get(`.article-preview:eq(${index})`).as(indexedArticle);
+  return Object.assign(article, {
+    title() {
+      return cy.get("@" + indexedArticle).find("h1");
+    },
+    description() {
+      return cy.get("@" + indexedArticle).find("p");
+    },
+    hasTags(list) {
+      list.map(tag => cy.get("@" + indexedArticle).contains(".tag-pill", tag));
+    },
+    tags() {
+      return cy
+        .get("@" + indexedArticle)
+        .find(".tag-pill")
+        .then(tags => tags.map((i, tag) => tag.innerText).toArray());
+    },
+    meta() {
+      const meta = cy
+        .get("@" + indexedArticle)
+        .find(".article-meta")
+        .as(indexedArticle + "meta");
+      return Object.assign(meta, {
+        avatar() {
+          return cy.get("@" + indexedArticle + "meta").find("img");
+        },
+        author() {
+          return cy.get("@" + indexedArticle + "meta").find(".author");
+        },
+        date() {
+          return cy.get("@" + indexedArticle + "meta").find(".date");
+        }
+      });
+    },
+    favoriteButton() {
+      const button = cy
+        .get("@" + indexedArticle)
+        .find("[data-test='favorite-count']")
+        .as(indexedArticle + "favoriteButton");
+      return Object.assign(article, button, {
+        isUnfavorited() {
+          return cy
+            .get("@" + indexedArticle + "favoriteButton")
+            .should("have.class", "unfavorited");
+        },
+        isFavorited() {
+          return cy
+            .get("@" + indexedArticle + "favoriteButton")
+            .should("have.class", "favorited");
+        }
+      });
+    }
+  });
+};
 
 describe("articles", () => {
   context("anonymous E2E", () => {
@@ -67,32 +167,41 @@ describe("articles", () => {
       cy.visit("/");
     });
 
-    it("provide articles preview", () => {
+    it.only("provide articles preview", () => {
       cy.get(".article-preview").should("have.length", 2);
 
-      article(0)
-        .should("contain", "Unfavorited Article Title")
-        .and("contain", "Fri Jul 12 2019")
-        .and("contain", "Unfavorited Article Description");
-      article(0)
-        .find("[data-test='favorite-count']")
-        .should("have.class", "unfavorited")
-        .should("contain", 0);
-      article(0)
-        .find("img")
-        .should("be.visible");
+      const firstArticle = articleFromList(0);
+      const firstArticleMeta = firstArticle.meta();
 
-      article(1)
-        .should("contain", "Favorited Article Title")
-        .and("contain", "Sat Jul 13 2019")
-        .and("contain", "Favorited Article Description");
-      article(1)
-        .find("[data-test='favorite-count']")
-        .should("have.class", "favorited")
-        .should("contain", 200);
-      article(1)
-        .find("img")
-        .should("be.visible");
+      firstArticle
+        .favoriteButton()
+        .should("contain", 0)
+        .isUnfavorited();
+      firstArticle.title().should("contain", "Unfavorited Article Title");
+      firstArticle
+        .description()
+        .should("contain", "Unfavorited Article Description");
+      firstArticle.hasTags(["tag1", "tag2"]);
+      firstArticle.tags().should("deep.equal", ["tag1", "tag2"]);
+      firstArticleMeta.avatar().should("be.visible");
+      firstArticleMeta.author().should("contain", "testuser1");
+      firstArticleMeta.date().should("contain", "Fri Jul 12 2019");
+
+      const secondArticle = articleFromList(1);
+      const secondArticleMeta = secondArticle.meta();
+
+      secondArticle
+        .favoriteButton()
+        .should("contain", 200)
+        .isFavorited();
+      secondArticle.title().should("contain", "Favorited Article Title");
+      secondArticle
+        .description()
+        .should("contain", "Favorited Article Description");
+      secondArticle.hasTags(["tag3"]);
+      secondArticle.tags().should("deep.equal", ["tag3"]);
+      secondArticleMeta.author().should("contain", "testuser2");
+      secondArticleMeta.date().should("contain", "Sat Jul 13 2019");
     });
   });
 
