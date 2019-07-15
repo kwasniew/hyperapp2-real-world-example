7653b09ca3c144a6f89e7c58933ec5a4354a5d8c
kwasniew <kwasniewski.mateusz@gmail.com>
2019-07-15 15:03:13 +0200

    3 test modes: record, replay/stub and prod only

--- cypress/support/mountebank.js
+++ cypress/support/mountebank.js
@@ -1,11 +1,17 @@
 const normalize = s => s.replace(/[^a-z0-9]/gi, "_");
 
-const mode = Cypress.env("MODE");
+const mode = Cypress.env("mode");
 
 beforeEach(function() {
   // do it in before so that we can debug after the test runs
   // debug URL: http://localhost:2525/imposters
-  cy.exec('curl -X "DELETE" http://localhost:2525/imposters');
+  if(mode == "record" || mode == "stub") {
+    cy.exec('curl -X "DELETE" http://localhost:2525/imposters');
+    cy.on("window:before:load", win => {
+      win.apiUrl = Cypress.env("apiUrl");
+    });
+  }
+
   if (mode === "record") {
     cy.exec(
       "curl -d@cypress/support/mountebank.json http://localhost:2525/imposters"
@@ -18,11 +24,6 @@ beforeEach(function() {
     );
   }
 
-  // cy.on("window:before:load", e => {
-  //   console.log(" before load ");
-  //   window.API_ROOT = "http://localhost:3000/api";
-  // });
-  // cy.log("I run before every test in every spec file!!!!!!");
 });
 
 afterEach(function() {
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const API_ROOT= "https://conduit.productionready.io/api";
-export const API_ROOT = "http://localhost:3000/api";
+export const API_ROOT= window.apiUrl || "https://conduit.productionready.io/api";
+// export const API_ROOT = "http://localhost:3000/api";
