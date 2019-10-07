4f033f19d1d6288adc5c28372109c18473dc10bd
kwasniew <kwasniewski.mateusz@gmail.com>
2019-10-07 16:21:33 +0200

    cleaning up tests

--- test/articlesList.spec.js
+++ test/articlesList.spec.js
@@ -6,24 +6,26 @@ import { init } from "../src/app.js";
 import globalFeed from "./fixtures/global.json";
 import fetch from "node-fetch";
 
-const element = (name, container = document) => container.querySelector(`[data-test=${name}]`);
-const elements = (name, container = document) => container.querySelectorAll(`[data-test=${name}]`);
-const feed = feedName => getByText(element("feeds"), feedName);
+const element = (container) => name => container.querySelector(`[data-test=${name}]`);
+const pageElement = element(document);
+const elements = container => name => container.querySelectorAll(`[data-test=${name}]`);
+const pageElements = elements(document);
+const feed = feedName => getByText(pageElement("feeds"), feedName);
 const assertFeedActive = feedName => assert.ok(Array.from(feed(feedName).classList).includes("active"));
 const assertFeedInactive = feedName => assert.ok(!Array.from(feed(feedName).classList).includes("active"));
 const assertFeeds = (...feeds) => {
-  assert.deepStrictEqual(elements("feed").length, feeds.length);
+  assert.deepStrictEqual(pageElements("feed").length, feeds.length);
   feeds.map(feed => {
     Array.isArray(feed) ? assertFeedActive(feed[0]) : assertFeedInactive(feed);
   });
 };
-const article = index => elements("article")[index];
+const article = index => pageElements("article")[index];
 const unfavorited = element => Array.from(element.classList).includes("unfavorited");
 const favorited = element => Array.from(element.classList).includes("favorited");
 const activePage = () => document.querySelector(".active .page-link");
 const page = label => getByText(document.querySelector(".pagination"), label);
-const tag = (index) => elements("tag")[index];
-
+const tag = (index) => pageElements("tag")[index];
+const tags = (container) => Array.from(elements(container)("tag")).map(x => x.textContent);
 
 const apiUrl = "https://conduit.productionready.io/api";
 
@@ -48,82 +50,98 @@ describe("articles", function() {
   beforeEach(function() {
     jsdom.beforeEach();
   });
-  context("logged-in", function() {
-    beforeEach(async function() {
-      await login();
-    });
-    it("favorite/unfavorite articles", async function() {
+  context("stubbed response", function() {
+    beforeEach(function() {
       const { server } = this.polly;
       server.get(`${apiUrl}/articles?limit=10&offset=0`).intercept((req, res) => {
         res.status(200).json(globalFeed);
       });
       init();
-      const link = await waitForElement(() => feed("Global Feed"), { timeout: 1500 });
-      link.click();
-
+    });
+    it("favorite/unfavorite articles", async function() {
       await wait(
         () => {
           assert.deepStrictEqual(document.querySelectorAll(".article-preview").length, 2);
-        },
-        { timeout: 1500 }
+        }
       );
 
-      const button1 = element("favorite-count", article(0));
-      const button2 = element("favorite-count", article(1));
+      const button1 = element(article(0))("favorite-count");
+      const button2 = element(article(1))("favorite-count");
       assert.ok(unfavorited(button1));
       assert.ok(favorited(button2));
     });
+    it("provide articles preview", () => {
+      return wait(
+        () => {
+          const article0 = element(article(0));
+          assert.deepStrictEqual(article0("title").textContent, "Unfavorited Article Title");
+          assert.deepStrictEqual(article0("description").textContent, "Unfavorited Article Description");
+          assert.deepStrictEqual(tags(article(0)), ["tag1", "tag2"]);
+          assert.deepStrictEqual(article0("date").textContent, "Fri Jul 12 2019");
+          assert.deepStrictEqual(article0("avatar").src, "https://static.productionready.io/images/smiley-cyrus.jpg");
 
-    it("switch your and global feed", async () => {
+          const article1 = element(article(1));
+          assert.deepStrictEqual(article1("title").textContent, "Favorited Article Title");
+          assert.deepStrictEqual(article1("description").textContent, "Favorited Article Description");
+          assert.deepStrictEqual(tags(article(1)), ["tag3"]);
+          assert.deepStrictEqual(article1("date").textContent, "Sat Jul 13 2019");
+          assert.deepStrictEqual(article1("avatar").src, "https://dummyimage.com/100x100/f01818/ffffff&text=+boom");
+        }
+      );
+    });
+
+  });
+  context("logged-in", function() {
+    beforeEach(async function() {
+      await login();
       init();
+    });
+    it("switch your and global feed", async () => {
       await wait(() => {
         assertFeeds(["Your Feed"], "Global Feed");
-      }, {timeout: 1500});
+      });
       feed("Global Feed").click();
       await wait(() => {
         assertFeeds("Your Feed", ["Global Feed"]);
-      }, {timeout: 1500});
+      });
 
     });
 
     it("toggle tag feed when active", async () => {
-      init();
-      const firstTag = await waitForElement(() => {
-        return tag(0)
-      }, {timeout: 1500});
-      const text = firstTag.text;
+      const firstTag = await waitForElement(() => tag(0));
+      const text = firstTag.textContent;
       firstTag.click();
       await wait(() => {
         assertFeeds("Your Feed", "Global Feed", [text]);
-      }, {timeout: 1500});
+      });
       feed("Global Feed").click();
       await wait(() => {
         assertFeeds("Your Feed", ["Global Feed"]);
-      }, {timeout: 1500});
+      });
     });
 
     it("paginate articles", async function() {
-      init();
-      const link = await waitForElement(() => feed("Global Feed"), { timeout: 1500 });
+      const link = await waitForElement(() => feed("Global Feed"));
       link.click();
-      const currentPage = await waitForElement(() => activePage(), { timeout: 1500 });
-
+      await wait(() => {
+        assert.deepStrictEqual(activePage().textContent, "1");
+      });
       page("2").click();
       await wait(() => {
-        assert.deepStrictEqual(activePage().text, "2");
-      }, {timeout: 1500});
+        assert.deepStrictEqual(activePage().textContent, "2");
+      });
     });
   });
   context("guest", function() {
-    it("show active global feed with 10 articles", async function() {
+    beforeEach(async function() {
       init();
-
+    });
+    it("show active global feed with 10 articles", async function() {
       return wait(
         () => {
           assertFeeds(["Global Feed"]);
           assert.deepStrictEqual(document.querySelectorAll(".article-preview").length, 10);
-        },
-        { timeout: 1500 }
+        }
       );
     });
   });
