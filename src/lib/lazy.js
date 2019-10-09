c47fbcd9a7832af05eea2acb33f4db7a171eba36
kwasniew <kwasniewski.mateusz@gmail.com>
2019-10-09 23:22:44 +0200

    lazy docs

--- src/lib/lazy.js
+++ src/lib/lazy.js
@@ -1,21 +1,22 @@
+// const { HomePage, LoadHomePage } = lazy(() => import("../pages/home.js"));
+// HomePage(html`<div>Loading</div>`) - presence of an argument makes it a view function. Always returns sync value with either a preloaded view or a placeholder
+// LoadHomePage() - absence of arguments makes it a lazy action that always returns a promise
 export const lazy = loader => {
   let loadedModule;
-  const lazyActionOrView = function(name) {
-    return function lazy(placeholder) {
-      if (typeof placeholder !== "undefined") {
-        // view
-        return loadedModule ? loadedModule[name] : () => placeholder;
-      }
-      // action
-      if (loadedModule) {
-        return Promise.resolve(loadedModule[name]);
-      }
+  const lazyActionOrView = name => placeholder => {
+    if (typeof placeholder !== "undefined") {
+      // view
+      return loadedModule ? loadedModule[name] : () => placeholder;
+    }
+    // action
+    if (loadedModule) {
+      return Promise.resolve(loadedModule[name]);
+    }
 
-      return loader().then(module => {
-        loadedModule = module;
-        return module[name];
-      });
-    };
+    return loader().then(module => {
+      loadedModule = module;
+      return module[name];
+    });
   };
   return new Proxy(
     {},
