9af7979b1b66fe7e601c4475ff8d3a10a819ecb8
Mateusz Kwasniewski <kwasniew@MacBook-Pro.local>
2020-06-07 15:42:37 +0200

    pruning code experiments

--- test/assertions.js
+++
@@ -1,6 +0,0 @@
-import assert from "assert";
-import { pageElement } from "./selectors";
-
-export const assertElementValue = name => value => () => {
-  assert.deepStrictEqual(pageElement(name).value, value);
-};
--- test/selectors.js
+++
@@ -1,5 +0,0 @@
-export const element = container => name => container.querySelector(`[data-test=${name}]`);
-export const pageElement = element(document);
-export const elements = container => name => container.querySelectorAll(`[data-test=${name}]`);
-export const pageElements = elements(document);
-export const hasClass = element => className => Array.from(element.classList).includes(className);
