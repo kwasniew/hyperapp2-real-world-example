de105196d038e036357312bf7a76f2dd16663c20
Mateusz Kwasniewski <kwasniew@MacBook-Pro.local>
2020-06-07 13:30:21 +0200

    servor

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,6 @@ import { ProfilePage, LoadProfilePage, LoadProfileFavoritedPage } from "../pages
 
 import { ARTICLE, EDITOR, HOME, LOGIN, NEW_EDITOR, PROFILE, PROFILE_FAVORITED, REGISTER, SETTINGS } from "./links.js";
 import { fromEntries } from "../lib/object.js";
-import { lazy } from "../lib/lazy.js";
-
-// const { HomePage, LoadHomePage } = lazy(() => import("../pages/home.js"));
-// const { LoginPage, LoadLoginPage, RegisterPage, LoadRegisterPage } = lazy(() => import("../pages/auth.js"));
-// const { SettingsPage, LoadSettingsPage } = lazy(() => import("../pages/settings.js"));
-// const { EditorPage, LoadNewEditorPage, LoadEditorPage } = lazy(() => import("../pages/editor.js"));
-// const { ArticlePage, LoadArticlePage } = lazy(() => import("../pages/article.js"));
-// const { ProfilePage, LoadProfilePage, LoadProfileFavoritedPage } = lazy(() => import("../pages/profile.js"));
 
 const pageStructure = [
   [HOME, HomePage, LoadHomePage],
